df30180e2cf8cde49646d0ea5b0867117ea3d8e5
Walter Vides <omarvides@gmail.com>
2019-10-10 21:20:57 +0200

    Basic examples

---
+++ examples/hello.rb
@@ -0,0 +1,27 @@
+=begin
+You can define multiline comments by opening the block with "=begin"
+and ending it with =end each in a new line
+=end
+
+# Variables
+something = 1
+simple_array = []
+str_trfrm = "A simple string"
+
+simple_array[0] = 0
+simple_array[1] = 1
+simple_array[2] = 2
+simple_array[3] = 3
+
+# Strings and interpolation
+puts 'Printed in console'
+puts "String interpolation requires double quotes \"\" example: #{something}"
+
+# Strings operations
+puts str_trfrm.reverse
+puts str_trfrm.upcase
+puts str_trfrm.downcase
+puts str_trfrm.length
+
+# Arrays
+puts simple_array
