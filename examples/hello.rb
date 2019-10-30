e355b85bca223af38840f0aca44576ed85c9d203
Walter Vides <omarvides@gmail.com>
2019-10-30 21:30:33 +0100

    Adding more examples

--- examples/hello.rb
+++ examples/hello.rb
@@ -13,6 +13,16 @@ simple_array[1] = 1
 simple_array[2] = 2
 simple_array[3] = 3
 
+basic_hash = {
+  "firstValue" => "value 1",
+  "count" => 2,
+  "something" => "Nothing"
+}
+
+symbolHash = Hash.new()
+symbolHash[:Key] = "Simple"
+symbolHash[:Key2] = "Something here"
+
 # Strings and interpolation
 puts 'Printed in console'
 puts "String interpolation requires double quotes \"\" example: #{something}"
@@ -25,3 +35,8 @@ puts str_trfrm.length
 
 # Arrays
 puts simple_array
+
+puts basic_hash
+puts basic_hash["firstValue"]
+puts symbolHash[:Key]
+puts symbolHash[:Key2]
